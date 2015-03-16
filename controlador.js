5761d37e9de22faf5ba1336643672bcbbef9d283
jcgallegoss <josecarlos_182_6@hotmail.com>
2015-03-15 21:13:19 -0700

    s

--- controlador.js
+++ controlador.js
@@ -1,5 +1,7 @@
 var app = angular.module('GeekSoft',[]);
 app.controller('Empleados',['$scope','$http',function(s,http){
 	s.nombre="hola";
-	http.get('Trabajadores.json');
+	http.get('Trabajadores.json').success(function(data){
+		s.empleado=data;
+	});
 }]);
